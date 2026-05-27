61747325dab7f5dba881c039696e58dfbd3b018a
Hardikmaloo21 <hardikmaloo21@gmail.com>
2026-05-27 21:20:23 +0530

    projects

--- Week12/09-05-2026/assessment/OnlineFoodOrderingSystem/FoodOrderingMVC/FoodOrdering.Web/Program.cs
+++ Week12/09-05-2026/assessment/OnlineFoodOrderingSystem/FoodOrderingMVC/FoodOrdering.Web/Program.cs
@@ -8,8 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")!));
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
---
+++ Week12/09-05-2026/assessment/OnlineFoodOrderingSystem/TODO.md
@@ -0,0 +1,9 @@
+- [x] Update Program/appsettings for environment-driven DB config readiness
+- [x] Update docker-friendly connection default in FoodOrdering.Web/appsettings.json
+- [x] Add docker-compose.yml to run SQL Server + web
+- [x] Add .dockerignore to speed up Docker builds
+- [ ] Build: dotnet build (solution)
+- [ ] Build: docker compose build
+- [ ] Run: docker compose up -d
+- [ ] Smoke test: container logs + http://localhost:8080
+
